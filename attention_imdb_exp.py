f7e55c6900843a0f2a79654fea4e449f49a571f4
lxh5147 <lxh5147@gmail.com>
2016-07-19 23:51:37 +0800

    try to mimic the settings of naacl paper for imdb exp

--- attention_imdb_exp.py
+++ attention_imdb_exp.py
@@ -8,6 +8,7 @@ from attention_layer import check_and_throw_if_fail
 import numpy as np
 from keras.callbacks import EarlyStopping
 from attention_exp import faked_dataset
+from keras.optimizers import SGD
 # import tensorflow as tf
 
 # K.set_session(tf.Session())
@@ -33,7 +34,7 @@ def imdb_exp(max_sentences, max_words, sentence_output_dim, word_output_dim, sen
     model = build_classifier_with_hierarchical_attention(input_feature_dims[0], input_shape, input_feature_dims, output_dims, attention_weight_vector_dims, vocabulary_size, word_embedding_dim, initial_embedding,
                                                          use_sequence_to_vector_encoder, classifier_output_dim, classifier_hidden_unit_numbers, classifier_drop_out_rates)
     # compile the model
-    model.compile(optimizer='rmsprop', loss=categorical_crossentropy_ex, metrics=['accuracy'])
+    model.compile(optimizer=SGD(momentum=0.9)   , loss=categorical_crossentropy_ex, metrics=['accuracy'])
     # train
     total = 10
     batch_size = 2
@@ -56,20 +57,24 @@ def imdb_exp(max_sentences, max_words, sentence_output_dim, word_output_dim, sen
 
 
 if __name__ == '__main__':
+    # http://aclweb.org/anthology/N16-1174
     max_sentences = 10
     max_words = 15
-    sentence_output_dim = 25
-    word_output_dim = 18
+
+    sentence_output_dim = 50
+    word_output_dim = 50
+
     sentence_attention_weight_vec_dim = 5
     word_attention_weight_vec_dim = 8
+
     vocabulary_size = 100
-    word_embedding_dim = 16
+    word_embedding_dim = 200
     initial_embedding = np.random.random((vocabulary_size, word_embedding_dim))
     classifier_output_dim = 20
     classifier_hidden_unit_numbers = []
     classifier_drop_out_rates = []
+    # batch size = 64
     imdb_exp(max_sentences, max_words, sentence_output_dim, word_output_dim, sentence_attention_weight_vec_dim, word_attention_weight_vec_dim, vocabulary_size, word_embedding_dim, initial_embedding, classifier_output_dim, classifier_hidden_unit_numbers, classifier_drop_out_rates)
-
     # debug_softmax_layer()
     # debug_attention_with_classifier_layer()
     # debug_attention_layer()
